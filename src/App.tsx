51090f40551269b0528a2166ce5bf0ba8d1a6409
J.Boughattas <boughattasjassem@outlook.com>
2022-10-20 18:10:09 +0100

    initialize project with chakraui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Button, useColorMode } from "@chakra-ui/react";
+
 function App() {
-  return <div>App</div>;
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <div>
+      <div>app</div>
+      <Button onClick={toggleColorMode}>toggle color mode</Button>
+    </div>
+  );
 }
 
 export default App;
